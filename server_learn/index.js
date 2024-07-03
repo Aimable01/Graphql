d541157dd521a16b6a93e7fcb57dfa702c7cddc0
Aimable01 <aimable.kwizera14@gmail.com>
2024-07-03 08:49:57 +0200

    add resolvers

--- server_learn/index.js
+++ server_learn/index.js
@@ -1,15 +1,36 @@
 import { ApolloServer } from "@apollo/server";
 import { startStandaloneServer } from "@apollo/server/standalone";
 
-import { typeDefs } from "./schema";
+// db
+import _db from "./_db.js";
+
+import { typeDefs } from "./schema.js";
+
+const resolvers = {
+  Query: {
+    games() {
+      return _db.games;
+    },
+    reviews() {
+      return _db.reviews;
+    },
+    authors() {
+      return _db.authors;
+    },
+  },
+};
 
 // server setup
 const server = new ApolloServer({
   typeDefs,
-  // resolvers
+  resolvers,
 });
+
 const { url } = await startStandaloneServer(server, {
   listen: { port: 4000 },
 });
 
-console.log(` 🚀 Server up on port 4000 `);
+console.log(`
+        🚀 Server up on port 4000 
+        📪 http://localhost:4000
+`);
