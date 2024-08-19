421cb2cef7e4c2ed2496349718591adb5c634abb
Aimable01 <aimable.kwizera14@gmail.com>
2024-08-19 21:36:05 +0200

    type definitions and query resolvers

--- learn/schema/resolvers.js
+++ learn/schema/resolvers.js
@@ -1,9 +1,35 @@
-const { UserList } = require("../FakeData");
+const { UserList, MovieList } = require("../FakeData");
+const _ = require("lodash");
 
 const resolvers = {
   Query: {
-    users() {
+    // user resolvers
+    users: () => {
       return UserList;
+    },
+    user: (parent, args) => {
+      const id = Number(args.id);
+      const user = _.find(UserList, { id });
+      return user;
+    },
+
+    // movie resolvers
+    movies: () => {
+      return MovieList;
+    },
+    movie: (parent, args) => {
+      const name = args.name;
+      const movie = _.find(MovieList, { name });
+      return movie;
+    },
+  },
+  User: {
+    favouriteMovies: () => {
+      return _.filter(
+        MovieList,
+        (movie) =>
+          movie.yearOfPublication >= 2000 && movie.yearOfPublication <= 2010
+      );
     },
   },
 };
